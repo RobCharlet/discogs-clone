b0cc4ddff345bb2a5fd6d9b731fd49fc7edba602
Robin Charlet <robin.charlet@laposte.net>
2020-09-09 17:44:21 +0200

    Configure record Doctrine mapping

---
+++ src/Library/Infra/Resources/Types/UuidType.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Library\Infra\Resources\Types;
+
+use Doctrine\DBAL\Platforms\AbstractPlatform;
+use Doctrine\DBAL\Types\ConversionException;
+use Doctrine\DBAL\Types\Type;
+use InvalidArgumentException;
+use Symfony\Component\Uid\Uuid;
+
+
+class UuidType extends Type
+{
+    const NAME = 'uuid';
+
+    public function getSQLDeclaration(array $column, AbstractPlatform $platform)
+    {
+        return $platform->getVarcharTypeDeclarationSQL($column);
+    }
+
+    public function convertToPHPValue($value, AbstractPlatform $platform)
+    {
+        if ($value === null || $value === '') {
+            return null;
+        }
+
+        if ($value instanceof Uuid) {
+            return $value;
+        }
+
+        try {
+            $uuid = Uuid::fromString($value);
+        } catch (InvalidArgumentException $e) {
+            throw ConversionException::conversionFailed($value, static::NAME);
+        }
+
+        return $uuid;
+    }
+
+    public function convertToDatabaseValue($value, AbstractPlatform $platform)
+    {
+        if ($value === null || $value === '') {
+            return null;
+        }
+
+        if (
+            $value instanceof Uuid
+            || (
+                (is_string($value)
+                    || method_exists($value, '__toString'))
+                && Uuid::isValid((string) $value)
+            )
+        ) {
+            return (string) $value;
+        }
+
+        throw ConversionException::conversionFailed($value, static::NAME);
+    }
+
+    public function getName()
+    {
+        return static::NAME;
+    }
+}
